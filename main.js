a50da101174fdbf0735f6f1c0ec81de329ddea6b
Paul <pacalabre@gmail.com>
2016-03-29 18:56:04 -0700

    updating commits

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 
+
 $('#myForm').submit(function(event) {
   event.preventDefault();
   $(this).children('input:text').each(function(index,elem) {
     var value = $(elem).val();
-    $('section').append('<h1>'+value+'</h1>'+'<button class="item-button">x</button>');
-    $(elem).val('');
+    var eachTask = $('section').append('<div class="task-style"><h1>'+value+'</h1>'+'<button class="item-button">Remove Task</button></div>');
   });
-  console.log(event);
-})
+  // $(this).children('input:text').each(function(index, elem) {
+  //   $('.task-style').submit(function(event){
+  //     console.log("this happened");
+  //   });
+
+  })
+
+  $('.item-button').click(function(event) {
+    event.preventDefault();
+    console.log('this');
+  })
+// })
+
+
+/*
+$('.item-button').click( function({
+  console.log("button was clicked");
+}))
+*/
