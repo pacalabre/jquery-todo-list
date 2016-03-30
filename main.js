d7e368a623cf550d841b7cb7b1c0a85c0a57d1e3
Paul <pacalabre@gmail.com>
2016-03-29 21:38:49 -0700

    finished styling

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $('#myForm').submit(function(event) {
   event.preventDefault();
   $(this).children('input:text').each(function(index,elem) {
     var value = $(elem).val();
-    var eachTask = $('section').append('<div class="task-style"><h1 class="task-style-h1">'+value+'</h1>'+'</div>');
+    var eachTask = $('section').append('<div class="task-style"><h1 class="task-style-h1">'+value+'<button class="task-button">X</button>'+'</h1>'+'</div>');
     //This would not work outside of the function
     $('div').click(function(event) {
     event.preventDefault();
