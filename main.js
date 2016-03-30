5213d6d47c5686ee1896e3e038188cdbdbebe6a2
Paul <pacalabre@gmail.com>
2016-03-29 19:19:06 -0700

    able to remove items from the list

--- main.js
+++ main.js
@@ -5,18 +5,20 @@ $('#myForm').submit(function(event) {
   $(this).children('input:text').each(function(index,elem) {
     var value = $(elem).val();
     var eachTask = $('section').append('<div class="task-style"><h1>'+value+'</h1>'+'<button class="item-button">Remove Task</button></div>');
-  });
-  // $(this).children('input:text').each(function(index, elem) {
-  //   $('.task-style').submit(function(event){
-  //     console.log("this happened");
-  //   });
-
-  })
-
-  $('.item-button').click(function(event) {
+    $('div').click(function(event) {
     event.preventDefault();
-    console.log('this');
+    $(this).remove();
+})
+  });
+
   })
+
+$('div').click(function(event) {
+  event.preventDefault();
+  console.log("this");
+  //$('this').remove();
+})
+
 // })
 
 
