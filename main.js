1c488feb2c6c637d1d24658fb60648059ca8786a
Paul <pacalabre@gmail.com>
2016-03-29 19:22:10 -0700

    removed buttom from being added

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $('#myForm').submit(function(event) {
   event.preventDefault();
   $(this).children('input:text').each(function(index,elem) {
     var value = $(elem).val();
-    var eachTask = $('section').append('<div class="task-style"><h1>'+value+'</h1>'+'<button class="item-button">Remove Task</button></div>');
+    var eachTask = $('section').append('<div class="task-style"><h1>'+value+'</h1>'+'</div>');
     $('div').click(function(event) {
     event.preventDefault();
     $(this).remove();
