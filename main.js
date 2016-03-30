247d801d77983cbc8da5203d08947a8113c5f632
Paul <pacalabre@gmail.com>
2016-03-29 19:33:05 -0700

    added another solution to on a different scope level

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ $('#myForm').submit(function(event) {
   $(this).children('input:text').each(function(index,elem) {
     var value = $(elem).val();
     var eachTask = $('section').append('<div class="task-style"><h1>'+value+'</h1>'+'</div>');
+    //This would not work outside of the function
     $('div').click(function(event) {
     event.preventDefault();
     $(this).remove();
     })
   });
 })
+
+ // This does work outside of the scope -- ask why above will not work, but this will work
+ // $('section').on('click', 'div', function(event) {
+ //    $(this).remove();
+ //    })
